3ce220f83cb61c3c93643a76d0085e47a38157c3
Nic Quintos <nicole.quintos@codeandtheory.com>
2024-09-05 19:05:21 +0800

    JS202: Return Elements using CSS Selectors

---
+++ app.js
@@ -0,0 +1,4 @@
+var navigationLinks = document.querySelectorAll("nav li a");
+var galleryLinks = document.querySelectorAll('#gallery li a');
+var footerImages = document.querySelectorAll('footer img');
+
