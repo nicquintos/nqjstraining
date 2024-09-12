423aeb7281f83620a8bb75a53d745fdfa06e74eb
Nic Quintos <nicole.quintos@codeandtheory.com>
2024-09-12 13:26:48 +0800

    JS203: Adding an Event Listener

---
+++ app.js
@@ -0,0 +1,5 @@
+var warning = document.getElementById("warning");
+var button = document.getElementById('makeItRed');
+button.addEventListener('click', function() {
+warning.style.backgroundColor = "red";
+});
