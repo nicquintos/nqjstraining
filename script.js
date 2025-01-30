81f3fedfdf980af3593ebc474f771eeb659ec02c
Nic Quintos <nicole.quintos@codeandtheory.com>
2025-01-30 16:34:37 +0800

    JS1062: JS102_Doing_Math_task

--- script.js
+++
@@ -1,7 +0,0 @@
-const wholesalePrice = 5.45;
-const retailPrice = 9.99;
-const quantity = 47;
-
-var salesTotal = retailPrice * quantity;
-var profit = salesTotal - (wholesalePrice * quantity);
-var profitPerUnit = profit / quantity;
