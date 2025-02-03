bc1b98a5c101d674a27a9dda24414962887f688a
Nic Quintos <nicole.quintos@codeandtheory.com>
2025-02-03 13:46:51 +0800

    JS1068: JS202 - Getting handled on the DOM challenge3

--- app.js
+++
@@ -1,4 +0,0 @@
-var navigationLinks = document.querySelectorAll("nav li a");
-var galleryLinks = document.querySelectorAll('#gallery li a');
-var footerImages = document.querySelectorAll('footer img');
-
