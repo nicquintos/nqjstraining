80d3adf53a10b029757f2071100f66180a3cc46e
Nic Quintos <nicole.quintos@codeandtheory.com>
2025-02-03 13:56:05 +0800

    JS1069: JS203 - responding to Events challenge

---
+++ JS1069_JS203_responding_to_events_challenge/app.js
@@ -0,0 +1,5 @@
+const warning = document.getElementById("warning");
+const button = document.getElementById('makeItRed');
+button.addEventListener('click', function() {
+warning.style.backgroundColor = "red";
+});
--- app.js
+++
@@ -1,5 +0,0 @@
-var warning = document.getElementById("warning");
-var button = document.getElementById('makeItRed');
-button.addEventListener('click', function() {
-warning.style.backgroundColor = "red";
-});
