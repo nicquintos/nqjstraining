b228647e8748f5922528974c0ad2fd041220f27f
Nic Quintos <nicole.quintos@codeandtheory.com>
2025-02-03 14:44:09 +0800

    JS1068: JS202 update 1

--- JS1068_JS202_Getting_handled_onthe_DOM_challenge2/app.js
+++ JS1068_JS202_Getting_handled_onthe_DOM_challenge2/app.js
@@ -2,6 +2,6 @@
 const listItems = document.getElementsByTagName('li');
 const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
 
-for(var i = 0; i < colors.length; i ++) {
+for(let i = 0; i < colors.length; i ++) {
   listItems[i].style.color = colors[i];    
 }
