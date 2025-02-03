3d165086ed78775c5b8e60297180ab732b060765
Nic Quintos <nicole.quintos@codeandtheory.com>
2025-02-03 13:29:52 +0800

    JS1068: JS202 - Getting handled on the DOM challenge2

---
+++ JS1068_JS202_Getting_handled_onthe_DOM_challenge2/app.js
@@ -0,0 +1,7 @@
+// Complete the challenge by writing JavaScript below
+const listItems = document.getElementsByTagName('li');
+const colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
+
+for(var i = 0; i < colors.length; i ++) {
+  listItems[i].style.color = colors[i];    
+}
--- app.js
+++
@@ -1,7 +0,0 @@
-// Complete the challenge by writing JavaScript below
-var listItems = document.getElementsByTagName('li');
-var colors = ["#C2272D", "#F8931F", "#FFFF01", "#009245", "#0193D9", "#0C04ED", "#612F90"];
-
-for(var i = 0; i < colors.length; i ++) {
-  listItems[i].style.color = colors[i];    
-}
