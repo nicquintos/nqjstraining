97c1101085c28a768d3292e2f22d70f2a5bf4276
Nic Quintos <nicole.quintos@codeandtheory.com>
2025-02-03 11:07:44 +0800

    JS1064: JS104 - Create a do while loop

--- JS1064_JS104_create_do_while_loop/script.js
+++ JS1064_JS104_create_do_while_loop/script.js
@@ -1,8 +1,7 @@
 // Display the prompt dialogue while the value assigned to `secret` is not equal to "sesame"
-let secret;
 let password = 'abc123';
 do {
-  secret = prompt("What is the secret password?");
+  prompt("What is the secret password?");
 } while ( password !== 'abc123' );
 
 // This should run after the loop is done executing
